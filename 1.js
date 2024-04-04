00954f5490815078af4d49b07fab436295bbec9f
yashitsme <yashitsme1@gmail.com>
2024-04-04 10:49:47 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,4 @@
+document.getElementById("editButton").addEventListener("click", function() {
+    // You can add your logic here for editing the profile
+    alert("Edit Profile button clicked!");
+});
